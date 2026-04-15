4604f7ba775010dfb116401c7c92000637eee0bf
shavkatovjavohir <shavkatovjavohir32@gmail.com>
2026-04-15 19:21:01 +0500

    club tayyor

--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
+import HouseImg from "./../assets/house.png";
+import HeaderImg from "./../assets/Header.png";
 
 const Club = () => {
   return (
-    <div>Club</div>
-  )
-}
+    <div className="max-w-[1440px] mx-auto font-sans bg-white text-black">
+      {/* Header */}
+      <header className="flex items-center justify-between px-6 py-4 border-b">
+        <img src={HeaderImg} alt="logo" className="h-8 text" />
 
-export default Club
+        <nav className="flex gap-6 text-sm">
+          <a href="#">О БРЕНДЕ</a>
+          <a href="#">ПРОДУКЦИЯ</a>
+          <a href="#">ПРЕИМУЩЕСТВА</a>
+          <a href="#">ОТКРЫТЬ КЛУБ</a>
+          <a href="#">КОНТАКТЫ</a>
+        </nav>
+
+        <button className="bg-cyan-500 text-white px-4 py-2 text-sm rounded">
+          ОСТАВИТЬ ЗАЯВКУ
+        </button>
+      </header>
+
+      {/* Banner */}
+      <div className="bg-cyan-500 text-white text-center py-6 text-xl font-semibold">
+        ОТКРЫТЬ КЛУБ ВМЕСТЕ С TRUE
+      </div>
+
+      {/* Image */}
+      <div className="flex justify-center py-8">
+        <img
+          src={HouseImg}
+          alt="club"
+          className="w-[1000px] max-w-full object-contain"
+        />
+      </div>
+
+      {/* Form */}
+      <div className="max-w-[500px] mx-auto py-10 px-4">
+        <h2 className="text-center text-xl font-semibold mb-6">
+          ОТПРАВИТЬ ЗАЯВКУ
+        </h2>
+
+        <div className="flex flex-col gap-4">
+          <input className="border-b p-2 outline-none" placeholder="ВАШЕ ИМЯ" />
+          <input className="border-b p-2 outline-none" placeholder="E-MAIL" />
+          <input
+            className="border-b p-2 outline-none"
+            placeholder="+998 (99) 999-99-99"
+          />
+          <input className="border-b p-2 outline-none" placeholder="ГОРОД" />
+
+          {/* Radio */}
+          <div>
+            <p className="text-sm mb-2">ХОТИТЕ ОТКРЫТЬ:</p>
+            <div className="flex flex-col gap-1 text-sm">
+              <label className="flex gap-2"><input type="radio" name="type" /> Фитнес клуб</label>
+              <label className="flex gap-2"><input type="radio" name="type" /> Йога клуб</label>
+              <label className="flex gap-2"><input type="radio" name="type" /> Персональная студия</label>
+            </div>
+          </div>
+
+          {/* Checkbox */}
+          <div>
+            <p className="text-sm mb-2">КАКИЕ УСЛУГИ ВАС ИНТЕРЕСУЮТ:</p>
+            <div className="flex flex-col gap-1 text-sm">
+              <label className="flex gap-2"><input type="checkbox" /> Консалтинг</label>
+              <label className="flex gap-2"><input type="checkbox" /> Маркетинг</label>
+              <label className="flex gap-2"><input type="checkbox" /> Подбор оборудования</label>
+            </div>
+          </div>
+
+          <textarea
+            className="border-b p-2 outline-none"
+            placeholder="КОММЕНТАРИЙ"
+          />
+
+          <button className="bg-cyan-500 text-white py-2 mt-4 rounded">
+            ОТПРАВИТЬ
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Club;
